0ab3fca0f2db3f512c03b72c836dc4d3eb6b1776
jammy-git <james@thecode.company>
2026-06-07 18:44:45 +0100

    feat: eleventy scaffold builds a placeholder page

---
+++ eleventy.config.js
@@ -0,0 +1,18 @@
+import CleanCSS from "clean-css";
+import { minify } from "html-minifier-terser";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts", "src/images": "images", "src/static": "/" });
+  eleventyConfig.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
+  eleventyConfig.addTransform("htmlmin", async function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      return minify(content, { collapseWhitespace: true, removeComments: true, minifyJS: true });
+    }
+    return content;
+  });
+  return {
+    dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+}
